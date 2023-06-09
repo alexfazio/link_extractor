99236f896ac09971f707db5fdd0389e74ca30ee1
alexfazio <alessandro.fazio@me.com>
2023-06-09 18:25:20 +0200

    09-06-2023_06-17

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if submit:
         href = link["href"]
         if href.endswith(extension):
             absolute_url: str = urljoin(url, href)
-            #st.write(absolute_url)
+            st.write(absolute_url)
             st.code(absolute_url, language="python", line_numbers=False)
 
 
