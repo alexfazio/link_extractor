d90e6090ace5bc3ff832e90b1e7753320a0bf006
alex <alessandro.fazio@me.com>
2023-06-30 17:07:41 +0200

    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.

--- my_functions.py
+++ my_functions.py
@@ -67,31 +67,12 @@ def rm(url, extension):
     # st.write(absolute_url)
     # st.code(absolute_url, language="python", line_numbers=False)
 
-def civitai(url, extension):
-    # Split the URL into parts
-    url_parts = url.split('/')
+import os
+from urllib.parse import urljoin
+import requests
+from bs4 import BeautifulSoup
 
-    # Extract the model ID from the URL
-    model_id = url_parts[url_parts.index('models') + 1]
-
-    # Construct the new URL
-    new_url = "https://civitai.com/api/download/models/" + model_id
-
-    # Construct the wget command
-    wget_command = "wget " + new_url + " --content-disposition"
-
-    return wget_command
-
-
-if __name__ == "__main__":
-    # We expect the URL as the first argument to the script
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <url>")
-        sys.exit(1)
-
-    url = sys.argv[1]
-    print(transform_link_to_wget_command(url))
-
+def civitai (url, extension):
     """Returns URL as wget UNIX commands, by appending a wget command before each URL."""
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -100,16 +81,25 @@ if __name__ == "__main__":
 
     code = ""
 
-    # for link in links:
-    #     href = link["href"]
-    #     if href.endswith(extension):
-    #         absolute_url = urljoin(url, href)
-    #         # Extract filename from path
-    #         filename = os.path.basename(absolute_url)
-    #         # Create UNIX command
-    #         unix_command = f'rm {filename}'
-    #         code += unix_command + '\n'
+    for link in links:
+        href = link["href"]
+        if href.startswith('https://civitai.com/api/download/models/'):
+            # Split the URL into parts
+            url_parts = href.split('/')
+            # Extract the model ID from the URL
+            model_id = url_parts[url_parts.index('models') + 1]
+            # Construct the new URL
+            new_url = "https://civitai.com/api/download/models/" + model_id
+            # Construct the wget command
+            unix_command = "wget " + new_url + " --content-disposition"
+            code += unix_command + '\n'
+        elif href.endswith(extension):
+            absolute_url = urljoin(url, href)
+            # Extract filename from path
+            filename = os.path.basename(absolute_url)
+            # Create UNIX command
+            unix_command = f'wget {filename}'
+            code += unix_command + '\n'
 
     st.code(code, language='python')
-    # st.write(absolute_url)
-    # st.code(absolute_url, language="python", line_numbers=False)
+
